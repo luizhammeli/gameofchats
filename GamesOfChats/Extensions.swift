affec574ee2f9e932b9dd600311bc723e2108f5e
Luiz Alfredo Diniz Hammerli <luizhammerli@gmail.com>
2017-04-09 12:06:55 -0300

    second commit

---
+++ GamesOfChats/Extensions.swift
@@ -0,0 +1,9 @@
+//
+//  Extensions.swift
+//  GamesOfChats
+//
+//  Created by Luiz Alfredo Diniz Hammerli on 08/04/17.
+//  Copyright © 2017 Luiz Alfredo Diniz Hammerli. All rights reserved.
+//
+
+import Foundation
